3a35d29c979cebec65bd1d99a41719d8bff8a180
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2022-05-12 22:26:11 +0300

    v. 1.0.16 - wtf

--- src/security/permission/permission-index.constants.ts
+++ src/security/permission/permission-index.constants.ts
@@ -2,8 +2,8 @@ import {ESystemPermissionNames} from "./system-permission-names.enum";
 import {EPermissionTypes} from "./permission-types.enum";
 
 export const SYSTEM_PERMISSION_INDEXES = {
-    [ESystemPermissionNames.ManageFields]: `${ESystemPermissionNames.ManageFields}.${EPermissionTypes.System}`,
-    [ESystemPermissionNames.ManageObjects]: `${ESystemPermissionNames.ManageObjects}.${EPermissionTypes.System}`,
     [ESystemPermissionNames.Invite]: `${ESystemPermissionNames.Invite}.${EPermissionTypes.System}`,
     [ESystemPermissionNames.Fire]: `${ESystemPermissionNames.Fire}.${EPermissionTypes.System}`,
+    [ESystemPermissionNames.ManageFields]: `${ESystemPermissionNames.ManageFields}.${EPermissionTypes.System}`,
+    [ESystemPermissionNames.ManageObjects]: `${ESystemPermissionNames.ManageObjects}.${EPermissionTypes.System}`,
 }
