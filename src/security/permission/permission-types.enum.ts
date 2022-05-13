7fd10fd0063bc6162092cab86e1ed632a13b2f88
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2022-05-13 15:27:39 +0300

    v. 1.0.19 - dupl non const enum

--- src/security/permission/permission-types.enum.ts
+++ src/security/permission/permission-types.enum.ts
@@ -1,4 +1,11 @@
-export const enum EPermissionTypes {
+export enum EPermissionTypes {
+    System = 'System',
+    Object = 'Object',
+    Feature = 'Feature',
+    Record = 'Record'
+}
+
+export const enum ECPermissionTypes {
     System = 'System',
     Object = 'Object',
     Feature = 'Feature',
